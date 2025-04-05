f810a491db296317b3d55b5728749c5210017a02
Rafay Syed <65063591+eskitete@users.noreply.github.com>
2025-04-04 19:41:41 -0500

    card fix

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -34,19 +34,22 @@ export const ContainerScroll = ({
     return [0.7, 0.7];
   };
 
-  const rotate = useTransform(scrollYProgress, [0, 1], [15, 0]);
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.7]);
-  const translate = useTransform(scrollYProgress, [0, 1], [0, -50]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [25, 0]);
+  const scale = useTransform(scrollYProgress, [0, 1], [1.2, 1]);
+  const translate = useTransform(scrollYProgress, [0, 1], [-2, -5]);
 
   return (
     <div
-      className="min-h-[60rem] md:min-h-[70rem] lg:min-h-[80rem] flex items-center justify-center relative p-2 sm:p-4 md:p-10 lg:p-20"
+      className="min-h-[80rem] md:min-h-[90rem] lg:min-h-[100rem] flex items-center justify-center relative p-2 sm:p-4 md:p-10 lg:p-20"
       ref={containerRef}
+      style={{ position: 'relative' }}
     >
       <div
         className="py-5 sm:py-10 md:py-20 w-full relative"
         style={{
-          perspective: "1000px",
+          perspective: "3000px",
+          transformStyle: "preserve-3d",
+          position: 'relative',
         }}
       >
         <Header translate={translate} titleComponent={titleComponent} scale={scale} />
@@ -65,7 +68,7 @@ export const Header = ({ translate, titleComponent, scale }: any) => {
         translateY: translate,
         scale,
       }}
-      className="div max-w-7xl mx-auto text-center mb-4 sm:mb-8 px-4"
+      className="div max-w-7xl mx-auto text-center mb-16 sm:mb-20 px-4"
     >
       {titleComponent}
     </motion.div>
@@ -88,14 +91,20 @@ export const Card = ({
       style={{
         rotateX: rotate,
         scale,
+        transformStyle: "preserve-3d",
         boxShadow:
           "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
       }}
-      className="max-w-[70rem] mx-auto w-full aspect-[16/9] sm:aspect-[16/10] md:aspect-[16/9] border-4 sm:border-8 border-[#222222] rounded-[20px] sm:rounded-[30px] shadow-2xl"
+      className="max-w-[75rem] mx-auto w-full aspect-[16/9] sm:aspect-[16/10] md:aspect-[16/9] border-4 sm:border-8 border-[#222222] rounded-[20px] sm:rounded-[30px] shadow-2xl"
     >
-      <div className="h-full w-full overflow-hidden rounded-[16px] sm:rounded-[22px] bg-gray-100 dark:bg-zinc-900">
+      <motion.div 
+        className="h-full w-full overflow-hidden rounded-[16px] sm:rounded-[22px] bg-gray-100 dark:bg-zinc-900"
+        style={{
+          transformStyle: "preserve-3d",
+        }}
+      >
         {children}
-      </div>
+      </motion.div>
     </motion.div>
   );
 }; 
